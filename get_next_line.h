6eddcbd690dd39a4feeaea700739ad6f04e81d34
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2019-06-11 14:12:31 +0200

    Initial Commit

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/11 13:59:52 by mcarter           #+#    #+#             */
+/*   Updated: 2019/06/11 14:11:19 by mcarter          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <fcntl.h>
+# include <stdlib.h>
+# define BUF_SIZE 32
+
+int		get_next_line(const int fd, char **line);
+#endif
