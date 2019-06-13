b1dd743585d82ab2c690a474185511ef69f2c913
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2019-06-13 16:02:16 +0200

    fixed norme issues

--- get_next_line.c
+++ get_next_line.c
@@ -6,53 +6,67 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 13:59:47 by mcarter           #+#    #+#             */
-/*   Updated: 2019/06/13 12:34:37 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/06/13 15:54:12 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		get_next_line(const int fd, char **line)
+static int	get_new_buffer(const int fd, char *buf, int *cur_pos)
+{
+	int	ret;
+
+	*cur_pos = 0;
+	ret = read(fd, buf, BUFF_SIZE);
+	buf[ret] = '\0';
+	if (errno)
+		return (-1);
+	if (ret == 0)
+		return (0);
+	return (1);
+}
+
+static void	set_line(char **tmp, char *buf, int *cur_pos, char ***line)
+{
+	int	len;
+
+	len = ft_strclen(buf + *cur_pos, '\n');
+	if (*tmp)
+	{
+		*tmp = ft_strnew(ft_strlen(**line) + len);
+		ft_strcpy(*tmp, **line);
+		free(**line);
+		ft_strncat(*tmp, buf + *cur_pos, len);
+	}
+	else
+	{
+		*tmp = ft_strnew(len);
+		ft_strncpy(*tmp, buf + *cur_pos, len);
+		(*tmp)[len] = '\0';
+	}
+	**line = *tmp;
+	*cur_pos = *cur_pos + len;
+}
+
+int			get_next_line(const int fd, char **line)
 {
 	int			ret;
 	char		*tmp;
 	static char	buf[BUFF_SIZE + 1];
 	static int	cur_pos = 0;
-	int			len;
 
 	tmp = NULL;
-	while (buf[cur_pos] != '\n') // || buf[cur_pos] == '\0')
+	while (buf[cur_pos] != '\n')
 	{
 		if (buf[cur_pos] == '\0')
 		{
-			cur_pos = 0;
-			ret = read(fd, buf, BUFF_SIZE);
-			buf[ret] = '\0';
-			if (errno)
-				return (-1);
-			if (ret == 0)
-				return (0);
+			if ((ret = get_new_buffer(fd, buf, &cur_pos)) != 1)
+				return (ret);
 		}
 		else
 			tmp = NULL;
-		len = ft_strclen(buf + cur_pos, '\n');
-		if (tmp)
-		{
-			tmp = ft_strnew(ft_strlen(*line) + len);
-			ft_strcpy(tmp, *line);
-			free(*line);
-			ft_strncat(tmp, buf + cur_pos, len);
-		}
-		else
-		{
-			tmp = ft_strnew(len);
-			ft_strncpy(tmp, buf + cur_pos, len);
-			tmp[len] = '\0';
-		}
-		*line = tmp;
-		cur_pos = cur_pos + len;
+		set_line(&tmp, buf, &cur_pos, &line);
 	}
-
 	if (buf[cur_pos] == '\n')
 	{
 		cur_pos++;
--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 15:35:57 by mcarter           #+#    #+#             */
-/*   Updated: 2019/06/12 11:33:31 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/06/13 13:03:50 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,31 @@
 #include "../get_next_line.h"
 #include <fcntl.h>
 
-int		main(int argc, char **argv)
+void	print_file(char *filepath)
 {
 	int		fd;
-	int		i;
 	char	*buf_ptr;
+
+	fd = open(filepath, O_RDONLY);
+	if (fd < 0)
+	{
+		ft_putstr_fd(filepath, 2);
+		ft_putendl_fd(": Error reading file", 2);
+	}
+	else
+	{
+		while (get_next_line(fd, &buf_ptr) == 1)
+		{
+			ft_putstr(buf_ptr);
+			ft_putendl(BMAGENTA "$" RESET);
+		}
+		close(fd);
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int		i;
 
 	if (argc == 1)
 	{
@@ -28,20 +48,7 @@ int		main(int argc, char **argv)
 	i = 1;
 	while (i < argc)
 	{
-		fd = open(argv[i], O_RDONLY);
-		if (fd < 0)
-		{
-			ft_putstr_fd(argv[i], 2);
-			ft_putendl_fd(": Error reading file", 2);
-		}
-		else
-		{
-			while (get_next_line(fd, &buf_ptr) == 1)
-			{
-				ft_putendl(buf_ptr);
-			}
-			close(fd);
-		}
+		print_file(argv[i]);
 		i++;
 	}
 	return (0);
