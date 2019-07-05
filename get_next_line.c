5e57e8f26b104460297f04e596c889516bc182d2
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2019-07-05 12:45:14 +0200

    rewrote to use lists (WIP)

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 13:59:47 by mcarter           #+#    #+#             */
-/*   Updated: 2019/07/01 10:33:54 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/07/05 12:29:16 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int	get_new_buffer(const int fd, STR buf)
+static int		get_new_buffer(const int fd, STR buf)
 {
 	int	ret;
 
@@ -26,88 +26,114 @@ static int	get_new_buffer(const int fd, STR buf)
 	return (1);
 }
 
-static void	shift_buffer(STR buf, int chars)
+static STR		get_line(t_finfo *finfo)
 {
-	int	i;
-
-	if (ft_strchr(buf, '\n'))
+	int	len;
+	int	ret;
+	STR	tmp;
+	STR	tmpnew;
+	
+	ret = 1;
+	tmp = NULL;
+	while (finfo->buff[finfo->pos] != '\n' && !finfo->eof)
 	{
-		i = 0;
-		while (i + chars < BUFF_SIZE)
+		if (finfo->buff[finfo->pos] == '\0')
 		{
-			buf[i] = buf[i + chars];
-			i++;
+			if ((ret = get_new_buffer(finfo->fd, finfo->buff)) == 0)
+			{
+				finfo->eof = 1;
+			}
 		}
-		buf[i] = '\0';
+		len = ft_strclen(finfo->buff + finfo->pos, '\n');
+		if (!tmp)
+		{
+			tmp = ft_strnew(len);
+			ft_strncpy(tmp, finfo->buff + finfo->pos, len);
+			tmp[len] = '\0';
+		}
+		else
+		{
+			tmpnew = ft_strnew(ft_strlen(tmp) + len);
+			ft_strcpy(tmpnew, tmp);
+			free(tmp);
+			ft_strncat(tmpnew, finfo->buff + finfo->pos, len);
+		}
+		finfo->pos += len;
 	}
+	finfo->pos++;
+	if (tmp)
+		return (tmp);
 	else
-		buf[0] = '\0';
+		return (ft_strnew(0));
 }
 
-static STR	read_line(STR buf)
+static t_finfo	*get_finfo_fd(t_list **lst, int fd)
 {
-	int	len;
-	STR	tmp;
+	t_finfo	*finfo;
+	t_list	*lst_tmp;
+	t_list	*lst_prev;
 
-	len = ft_strclen(buf, '\n');
-	tmp = ft_strnew(len);
-	ft_strncpy(tmp, buf, len);
-	tmp[len] = '\0';
-	shift_buffer(buf, len);
-	return (tmp);
+	lst_tmp = *lst;
+	while (lst_tmp)
+	{
+		if (((t_finfo *)(lst_tmp->content))->fd == fd)
+			return ((t_finfo *)lst_tmp->content);
+		lst_prev = lst_tmp;
+		lst_tmp = lst_tmp->next;
+	}
+	finfo = (t_finfo *)malloc(sizeof(t_finfo));
+	finfo->fd = fd;
+	finfo->buff = ft_strnew(BUFF_SIZE + 1);
+	finfo->pos = 0;
+	finfo->eof = 0;
+	if (lst_prev)
+		lst_prev->next = ft_lstnew(finfo, sizeof(t_finfo));
+	else
+		*lst = ft_lstnew(finfo, sizeof(t_finfo));
+	ft_memdel((void **)&finfo);
+	if (lst_prev)
+		return ((t_finfo *)lst_prev->next->content);
+	else
+		return ((t_finfo *)((*lst)->content));
 }
 
-static STR	concat_line(STR buf, STR old_line)
+static void		lstdelfnc(void *content, size_t size)
 {
-	int	len;
-	STR	tmp;
-
-	len = ft_strclen(buf, '\n');
-	tmp = ft_strnew(ft_strlen(old_line) + len);
-	ft_strcpy(tmp, old_line);
-	free(old_line);
-	ft_strncat(tmp, buf, len);
-	shift_buffer(buf, len);
-	return (tmp);
+	ft_memdel((void **)&(((t_finfo *)content)->buff));
+	ft_memdel(&content);
+	size++;
 }
 
 /*
-** int get_new_buffer(fd, buf) sets buffer and returns exit code
-** shift_buffer(buf, chars) moves the buffer up `chars`
-** str read_line(buf) returns malloced string
-** str concat_line(buf, old_line) returns malloced string, frees original
-** buf[BUFF_SIZE + 1] = flag whether file has finished reading
+** list.content (t_finfo) -> fd, buff, pos, eof
+** int     get_new_buffer(fd, buf) sets buffer and returns exit code
+** str     get_line(finfo) gets the line for the given fileinfo
+** t_finfo get_finfo_fd(lst_start, fd) gets the fileinfo for the given fd
+** lstdelfnc(void *, size_t) function to pass to ft_lstdelone
+**
+**gnl{ static lst_start; lst_cur }
 */
 
 int			get_next_line(const int fd, STR *line)
 {
-	int			ret;
-	static char	buf[BUFF_SIZE + 2];
-
-	if (fd == 1 || fd == 2)
-		return (-1);
-	if (buf[BUFF_SIZE + 1] == 1)
+	static t_list	*lst;
+	t_list			*tmp;
+	t_list			*tmp_prev;
+	t_finfo			*finfo;
+	
+	finfo = get_finfo_fd(&lst, fd);
+	if (finfo->eof)
 	{
-		buf[BUFF_SIZE + 1] = 0;
+		tmp = lst;
+		while (((t_finfo *)(tmp->content))->fd != fd)
+		{
+			tmp_prev = tmp;
+			tmp = tmp->next;
+		}
+		tmp_prev->next = tmp->next;
+		ft_lstdelone(&tmp, &lstdelfnc);
 		return (0);
 	}
-	if (*buf == '\0')
-		if ((ret = get_new_buffer(fd, buf)) != 1)
-		{
-			buf[BUFF_SIZE + 1] = 0;
-			return (ret);
-		}
-	*line = read_line(buf);
-	while (*buf == '\0' && buf[BUFF_SIZE + 1] != 1)
-	{
-		ret = get_new_buffer(fd, buf);
-		if (ret == -1)
-			return (ret);
-		else if (ret == 0)
-			buf[BUFF_SIZE + 1] = 1;
-		*line = concat_line(buf, *line);
-	}
-	if (*buf == '\n')
-		ft_memmove(buf, buf + 1, BUFF_SIZE);
+	*line = get_line(finfo);
 	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 13:59:52 by mcarter           #+#    #+#             */
-/*   Updated: 2019/06/12 18:00:32 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/07/05 12:15:48 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,14 @@
 # include <errno.h>
 # include "libft/libft.h"
 # define BUFF_SIZE 32
+
+typedef struct	s_finfo
+{
+	int	fd;
+	STR	buff;
+	int	pos;
+	int	eof;
+}				t_finfo;
 
 int		get_next_line(const int fd, STR *line);
 #endif
